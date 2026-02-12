3859f652254757244d56ca72f3e5038eef09f2af
Garvit Rangi <garvitrangi21@gmail.com>
2026-02-12 15:29:43 +0530

    pre final

--- src/Actions/WeatherLogic.tsx
+++ src/Actions/WeatherLogic.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Weather from "../Components/Weather";
+import cloudy from '../Assets/cloudy.png';
+import cloudymix from '../Assets/cloudymix.png';
+import heavyrain from '../Assets/heavyrain.png';
+import humidity from '../Assets/humidity.png';
+import sun from '../Assets/sun.png';
+import wind from '../Assets/wind.png';
+import rainyday from '../Assets/rainyday.png';
 
 interface WeatherResponse {
-  name: string;
+  location: string;
   main: {
     temp: number;
     humidity: number;
@@ -10,30 +17,69 @@ interface WeatherResponse {
   wind: {
     speed: number;
   };
+  weather:{
+    icon: string;
+  }
 }
+
 
 function WeatherLogic() {
   const [weatherData, setWeatherData] = useState<WeatherResponse | null>(null);
 
+  const allIcons = {
+    "01d": sun,
+    "01n": sun,
+    "02d": cloudymix,
+    "02n": cloudymix,
+    "03d": cloudy,
+    "03n": cloudy,
+    "04d": cloudy,
+    "04n": cloudy,
+    "09d": heavyrain,
+    "09n": heavyrain,
+    "10d": rainyday,
+    "10n": rainyday,
+    "11d": heavyrain,
+    "11n": heavyrain,
+    "13d": cloudymix,
+    "13n": cloudymix,
+
+  };
   const search = async (city: string) => {
+    if(city === ""){
+      alert("Please enter a city name");
+      return;  
+    }
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_ID}&units=metric`;
 
       const response = await fetch(url);
       const data = await response.json();
-
-      if (response.ok) {
-        setWeatherData(data);
-      } else {
-        console.log(data.message);
-      }
+        if(!response.ok){
+          alert("City not found");
+          return;
+        }
+        const icon: string | undefined = allIcons[data.weather[0].icon as keyof typeof allIcons];
+        setWeatherData({
+          location: data.name,
+          main: {
+            humidity: data.main.humidity,
+            temp: Math.floor(data.main.temp),
+          },
+          wind: {
+            speed: data.wind.speed,
+          },
+          weather: {
+            icon: icon
+          }
+        });
     } catch (error) {
       console.log("Error in loading Data");
     }
   };
 
   useEffect(() => {
-    search("London");
+    search("New Delhi"); 
   }, []);
 
   return <Weather weatherData={weatherData} onSearch={search} />;
--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import cloudy from '../Assets/cloudy.png';
 import cloudymix from '../Assets/cloudymix.png';
 import heavyrain from '../Assets/heavyrain.png';
@@ -9,39 +9,57 @@ import rainyday from '../Assets/rainyday.png';
 import './Weather.css';
 import SearchIcon from '@mui/icons-material/Search';
 
+interface WeatherResponse {
+    location: string;
+    main: {
+      temp: number;
+      humidity: number;
+    };
+    wind: {
+      speed: number;
+    };
+    weather:{
+      icon: string;
+    }
+  }
+
 interface Props {
-  weatherData: any;
+  weatherData: WeatherResponse | null;
   onSearch: (city: string) => void;
 }
 
 function Weather({ weatherData, onSearch }: Props) {
-  const [city, setCity] = useState("");
+const inputRef = useRef<HTMLInputElement | null>(null);
     
   return (
     <div className='weather'>
         <div className='search-bar'>
-            <input type="text" placeholder='Search city' className='search-input' />
-            <button className='search-button'><SearchIcon /></button>
+            <input ref = {inputRef} type="text" placeholder='Search city' className='search-input' />
+            <button className='search-button' onClick={() => onSearch(inputRef.current ? inputRef.current.value : '')}><SearchIcon /></button>
         </div>
-        <img src={sun} alt='cloudy' className='weather-icon'/>
-        <p className='temperature'> 18° C</p>
-        <p className='location'> London  </p>
+        {weatherData? <>
+            <img src={weatherData?.weather.icon} alt='cloudy' className='weather-icon'/>
+        <p className='temperature'> {weatherData?.main.temp}°C</p>
+        <p className='location'> {weatherData?.location}  </p>
         <div className='weathers-data'>
             <div className='col'>
                 <img src={humidity} alt='humidity' className='weather-detail-icon'/>
                 <div>
-                    <p> 60% </p>
+                    <p> {weatherData?.main.humidity} </p>
                     <span>Humidity</span>
                 </div>
             </div>
             <div className='col'>
                 <img src={wind} alt='wind' className='weather-detail-icon'/>
                 <div>
-                    <p> 10 km/h </p>
+                    <p> {weatherData?.wind.speed} km/h </p>
                     <span>Wind speed</span>
                 </div>
             </div>
         </div>    
+        </> : <></>
+        }
+        
     </div>
   )
 }
